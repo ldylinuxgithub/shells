a5b3296bf879d1629f06ae9b4267cd1265e63b75
ldygitee <ldygitee@gitee.com>
2020-03-25 22:54:49 +0800

    systemctl_start_nginx

---
+++ system_start_nginx.sh
@@ -0,0 +1,48 @@
+#!/bin/bash 
+#Author:丁丁历险(Jacob) 
+#本脚本编写完成后，放置在/etc/init.d/目录下，就可以被Linux系统自动识别到该脚本 
+#如果本脚本名为/etc/init.d/nginx，则service nginx start就可以启动该服务 
+#service nginx stop就可以关闭服务 
+#service nginx restart 可以重启服务 
+#service nginx status 可以查看服务状态 
+
+# 编写nginx启动脚本 
+ 
+program=/usr/local/nginx/sbin/nginx 
+pid=/usr/local/nginx/logs/nginx.pid 
+start(){ 
+if  [  -f   $pid  ];then
+         echo  "nginx服务已经处于开启状态" 
+else
+         $program 
+fi 
+stop(){ 
+if  [  -!  -f  $pid  ];then
+         echo "nginx服务已经关闭" 
+else
+         $program -s stop
+         echo  "关闭服务ok" 
+fi 
+} 
+status(){ 
+if  [  -f  $pid  ];then
+         echo "服务正在运行…" 
+else
+         echo "服务已经关闭" fi } 
+ 
+case   $1  in 
+start)
+       start;; 
+stop)
+       stop;; 
+restart)
+       stop
+       sleep 1
+       start;; 
+status)
+       status;; 
+*)
+       echo  "你输入的语法格式错误" 
+esac 
+ 
+ 
